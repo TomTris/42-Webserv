5506cc38e4280bee5d4d7fe23a0f3bb2b95d91ab
TomTris <quoctrido.jb@gmail.com>
2024-07-22 13:51:07 +0200

    added some status code

--- Tomweb.hpp
+++ Tomweb.hpp
@@ -44,12 +44,23 @@ void    	load_config(int ac, char **av, t_server_config &config);
 int	        socket_create(t_server_config &config, struct sockaddr_in &server_addr);
 std::string read_file(int &fd);
 
-void	set_init(fd_set &read_fds, t_server_data &server_data, int &max_fd, std::vector<int> &client_socket);
-void	handle_200(int &new_socket, std::string str);
+void    	set_init(fd_set &read_fds, t_server_data &server_data, int &max_fd, std::vector<int> &client_socket);
+void    	handle_200(int &new_socket, std::string str);
 
-void	handle_unknown_request(int &new_socket, std::string &path);
-void	handle_post_request(int	&new_socket, std::string &request_content, std::string &path);
-void	handle_delete_request(int &new_socket, std::string &path);
-void	handle_get_request(int &new_socket, std::string &path);
+void    	handle_unknown_request(int &new_socket, std::string &path);
+void    	handle_post_request(int	&new_socket, std::string &request_content, std::string &path);
+void    	handle_delete_request(int &new_socket, std::string &path);
+void    	handle_get_request(int &new_socket, std::string &path);
+
+std::string	header_200(void);
+void    	handle_204(int &new_socket);
+void    	handle_400(int &new_socket, std::string const &str);
+void    	handle_401(int &new_socket);
+void    	handle_403(int &new_socket);
+void    	handle_404(int &new_socket);
+void    	handle_408(int &new_socket);
+void    	handle_409(int &new_socket);
+void    	handle_411(int &new_socket);
+void	    handle_none(int &new_socket, int nbr, std::string const &str);
 
 #endif
--- handle_unknown_request.cpp
+++ handle_unknown_request.cpp
@@ -1,19 +1,110 @@
 #include "Tomweb.hpp"
 
-// about errcode, could be 404, forbidden, no permision, bla bla, later.
+std::string	header_200(void)
+{
+	std::string ret;
+	
+	ret = "HTTP/1.1 200 OK\r\n"
+				"Content-Type: text/html\r\n"
+				"\r\n";
+	return (ret);
+}
+
+//Request proccessed successfull
+void	handle_204(int &new_socket)
+{
+	std::string http = "HTTP/1.1 204 KO\r\nContent-Type: text/html\r\n\r\n<html><body>";
+	std::string http2 = "Request proccessed successful\n";
+	std::string http3 = "</body></html>";
+	http = http + http2 + http3;
+	if (write(new_socket, http.c_str(), http.length()) < 0)
+		throw std::runtime_error("write Failed");
+}
+
+//Bad request
+void	handle_400(int &new_socket, std::string const &str)
+{
+	std::string http = "HTTP/1.1 400 KO\r\nContent-Type: text/html\r\n\r\n<html><body>";
+	std::string http2 = "Bad Request\n";
+	std::string http3 = "</body></html>";
+	http = http + http2 + str + http3;
+	if (write(new_socket, http.c_str(), http.length()) < 0)
+		throw std::runtime_error("write Failed");
+}
+
+//Unauthorized
+void	handle_401(int &new_socket)
+{
+	std::string http = "HTTP/1.1 401 KO\r\nContent-Type: text/html\r\n\r\n<html><body>";
+	std::string http2 = "Permission denied\n";
+	std::string http3 = "</body></html>";
+	http = http + http2 + http3;
+	if (write(new_socket, http.c_str(), http.length()) < 0)
+		throw std::runtime_error("write Failed");
+}
+
+//Forbidden
+void	handle_403(int &new_socket)
+{
+	std::string http = "HTTP/1.1 403 KO\r\nContent-Type: text/html\r\n\r\n<html><body>";
+	std::string http2 = "Forbidden\n";
+	std::string http3 = "</body></html>";
+	http = http + http2 + http3;
+	if (write(new_socket, http.c_str(), http.length()) < 0)
+		throw std::runtime_error("write Failed");
+}
+
+//Not found
 void	handle_404(int &new_socket)
 {
 	char http_404[1024] = "HTTP/1.1 404 KO\r\n"
                                         "Content-Type: text/html\r\n"
                                         "\r\n"
-										"<html><body>404-File Not Found</body></html>";
+										"<html><body>404 Not Found</body></html>";
 	if (write(new_socket, http_404, strlen(http_404)) < 0)
 		throw std::runtime_error("write Failed");
 }
 
-void	handle_200(int &new_socket, std::string str)
+//Request Timeout
+void	handle_408(int &new_socket)
 {
-	std::string http = "HTTP/1.1 404 KO\r\nContent-Type: text/html\r\n\r\n<html><body>" + str + "</body></html>";
+	std::string http = "HTTP/1.1 408 KO\r\nContent-Type: text/html\r\n\r\n<html><body>";
+	std::string http2 = "Request Timeout\n";
+	std::string http3 = "</body></html>";
+	http = http + http2 + http3;
+	if (write(new_socket, http.c_str(), http.length()) < 0)
+		throw std::runtime_error("write Failed");
+}
+
+//Conflict
+void	handle_409(int &new_socket)
+{
+	std::string http = "HTTP/1.1 409 KO\r\nContent-Type: text/html\r\n\r\n<html><body>";
+	std::string http2 = "Conflict\n";
+	std::string http3 = "</body></html>";
+	http = http + http2 + http3;
+	if (write(new_socket, http.c_str(), http.length()) < 0)
+		throw std::runtime_error("write Failed");
+}
+
+//Content-Length required
+void	handle_411(int &new_socket)
+{
+	std::string http = "HTTP/1.1 411 KO\r\nContent-Type: text/html\r\n\r\n<html><body>";
+	std::string http2 = "Content-Length required\n";
+	std::string http3 = "</body></html>";
+	http = http + http2 + http3;
+	if (write(new_socket, http.c_str(), http.length()) < 0)
+		throw std::runtime_error("write Failed");
+}
+
+void	handle_none(int &new_socket, int nbr, std::string const &str)
+{
+	std::string http = "HTTP/1.1 ";
+	http += nbr;
+	http += " KO\r\nContent-Type: text/html\r\n\r\n<html><body>";
+	http += str;
+	http += "</body></html>";
 	if (write(new_socket, http.c_str(), http.length()) < 0)
 		throw std::runtime_error("write Failed");
 }
