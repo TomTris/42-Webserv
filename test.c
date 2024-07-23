0e0408bf458d76e8238f08a98169b3b4f4cb6648
Oleks <oleksandrbrittner@gmail.com>
2024-07-23 18:16:54 +0200

    fixed locations

--- test.c
+++ test.c
@@ -1,10 +1,25 @@
 #include "Tomweb.hpp"
 
+
+void handle_url(std::string& url)
+{
+    int ptr = 0;
+    while (url.substr(ptr).find('/') != std::string::npos)
+    {
+        ptr += url.substr(ptr).find('/');
+        ptr += 1;
+        int len = 0;
+        while (url.size() > ptr + len && url[ptr + len] == '/')
+            len++;
+        url.erase(ptr, len);
+    }
+	if (url[0] != '/')
+		url = "/" + url;
+}
+
 int	main(void)
 {
-	int fd= open("www////200.html", O_RDONLY);
-	if (fd > 0)
-		std::cout << "Gut" << std::endl;
-	else
-		std::cout << "bad " << std::endl;
+	std::string asd = "Asdas//asdasd///a";
+	handle_url(asd);
+	std::cout << asd << std::endl;
 }
