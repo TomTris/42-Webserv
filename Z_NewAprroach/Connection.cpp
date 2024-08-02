a123e148843c17f6c26ee92836758084148bc51c
Quoc Tri Do <qdo@2-G-9.42heilbronn.de>
2024-08-02 10:56:47 +0200

    OK

--- Z_NewAprroach/Connection.cpp
+++ Z_NewAprroach/Connection.cpp
@@ -2,7 +2,7 @@
 
 Connection::Connection(int fd) : socket_fd(fd), reader()
 {
-	std::cerr << "new Connect, socket, fd = " << socket_fd << ", " << fd << std::endl;
+	// std::cerr << "new Connect, socket, fd = " << socket_fd << ", " << fd << std::endl;
 	this->IsAfterResponseClose = 1;
 	this->readingHeaderDone = 0;
 	this->have_read = "";
